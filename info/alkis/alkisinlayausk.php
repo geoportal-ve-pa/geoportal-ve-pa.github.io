f4f6ea9daca2094305c93b62f31459004538659e
geoportal-ve-pa <christoph.blank@ba-pankow.berlin.de>
2025-04-11 14:18:11 +0200

    Add files via upload

---
+++ info/alkis/alkisinlayausk.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<meta name="author" content="b600352" >
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="alkisauszug.css">
+	<link rel="shortcut icon" type="image/x-icon" href="ico/Flurstueck.ico">
+	<title>ALKIS-Auskunft</title>
+	<script type="text/javascript">
+	function imFenster(dieURL) {
+		var link = encodeURI(dieURL);
+		window.open(link,'','left=10,top=10,width=680,height=800,resizable=yes,menubar=no,toolbar=no,location=no,status=no,scrollbars=yes');
+	}
+	</script>
+</head>
+<body><p class='err'>Kein Treffer fuer gml_id=0</p>
+<h2><img src='ico/Flurstueck.png' width='16' height='16' alt=''> Flurst&uuml;ck - &Uuml;bersicht</h2>
+<table class='outer'>
+<tr>
+<td>
+	<table class='kennzfs' title='Flurst&uuml;ckskennzeichen'>
+	<tr>
+		<td class='head'>Gmkg</td>
+		<td class='head'>Flur</td>
+		<td class='head'>Flurst-Nr.</td>
+	</tr>
+	<tr>
+		<td title='Gemarkung'></td>
+		<td title='Flurnummer'></td>
+		<td title='Flurst&uuml;cksnummer (Z&auml;hler / Nenner)'><span class='wichtig'></span></td>
+	</tr>
+	</table>
+</td>
+<td>
+	<p class='nwlink'>weitere Auskunft:<br>
+		<a href='javascript:imFenster("alkisfsnw.php?gkz=&amp;gmlid=0&amp;eig=j")' title='Flurst&uuml;cksnachweis'>Flurst&uuml;ck&nbsp;<img src='ico/Flurstueck_Link.png' width='16' height='16' alt=''></a><br>
+		<a href='javascript:imFenster("alkisfshist.php?gkz=&amp;gmlid=0")' title='Vorg&auml;nger des Flurst&uuml;cks'>Historie&nbsp;<img src='ico/Flurstueck_Historisch.png' width='16' height='16' alt=''></a><br>
+		<a href='javascript:imFenster("alkisgebaeudenw.php?gkz=&amp;gmlid=0")' title='Geb&auml;udenachweis'>Geb&auml;ude&nbsp;<img src='ico/Haus.png' width='16' height='16' alt=''></a>
+	</p>
+</td>
+</table>
+
+<p class='fsd'>Flurst&uuml;cksfl&auml;che: <b></b></p>
+
+<h2><img src='ico/Grundbuch_zu.png' width='16' height='16' alt=''> Grundbuch</h2></body>
+</html>
